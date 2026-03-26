f3275b96051d9a5c6f63190cc05bea488e95035f
(Messi) Junlin Wu <tallmessiwu@qq.com>
2026-03-26 14:29:03 +0800

    :sparkles: feat(wan22): add mxfp4 run scripts

---
+++ online_mxfp4.sh
@@ -0,0 +1,12 @@
+SGLANG_CACHE_DIT_FN=2
+SGLANG_CACHE_DIT_BN=1
+SGLANG_CACHE_DIT_WARMUP=4
+SGLANG_CACHE_DIT_RDT=0.4
+SGLANG_CACHE_DIT_MC=4
+SGLANG_CACHE_DIT_TAYLORSEER=true
+SGLANG_CACHE_DIT_TS_ORDER=2
+SGLANG_CACHE_DIT_ENABLED=true
+sglang generate --model-path /home/weights/Wan2.2-TI2V-5B-Diffusers \
+--quantization mxfp4 \
+--prompt "Two anthropomorphic cats in comfy boxing gear and bright gloves fight intensely on a spotlighted stage." \
+--height 704 --width 1280 --num-gpus 1 --num-frames 81 --num-inference-steps 40  --warmup

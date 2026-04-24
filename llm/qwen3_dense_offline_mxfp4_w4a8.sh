367c553a96404edb72b8d90106be1e60bd20cdc7
(Messi) Junlin Wu <tallmessiwu@qq.com>
2026-04-24 17:51:32 +0800

    :bug: fix: fix offline w4a8 script's weight path

--- llm/qwen3_dense_offline_mxfp4_w4a8.sh
+++ llm/qwen3_dense_offline_mxfp4_w4a8.sh
@@ -1,5 +1,5 @@
 sglang serve \
-    --model-path /home/weights/Qwen3-8B-dense-w4a8-040202 \
+    --model-path /home/weights/qwen3-8B-dense-w4a8-040202 \
     --host 0.0.0.0 \
     --port 6969 \
     --quantization modelslim \

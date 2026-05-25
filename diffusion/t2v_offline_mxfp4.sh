ada034284999e6539a00ec6de5892acff94b876f
Junlin Wu <tallmessiwu@qq.com>
2026-05-25 11:08:02 +0800

    :pencil2: fix(diffusion): correct Wan2.2-T2V mxfp4 weights dir casing

--- diffusion/t2v_offline_mxfp4.sh
+++ diffusion/t2v_offline_mxfp4.sh
@@ -7,6 +7,6 @@ SGLANG_CACHE_DIT_MC=4
 SGLANG_CACHE_DIT_TAYLORSEER=true
 SGLANG_CACHE_DIT_TS_ORDER=2
 SGLANG_CACHE_DIT_ENABLED=true
-sglang generate --model-path /home/weights/Wan2.2-T2V-A14B-Diffusers-mxfp4 \
+sglang generate --model-path /home/weights/Wan2.2-T2V-A14B-Diffusers-MXFP4 \
 --prompt "Two anthropomorphic cats in comfy boxing gear and bright gloves fight intensely on a spotlighted stage." \
 --height 704 --width 1280 --num-gpus 1 --num-frames 81 --num-inference-steps 40  --warmup
